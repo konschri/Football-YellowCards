7668b4e17c6321017d49c34ac34a25ee1090ff05
christantonis.kons <christantonis.kons@gmail.com>
2021-08-04 14:20:03 +0300

    solve 404 error

--- web-scraping-data/webscraper.py
+++ web-scraping-data/webscraper.py
@@ -2,17 +2,16 @@ import numpy as np
 import time
 import requests
 from bs4 import BeautifulSoup
-import csv
 import json
 import pandas as pd
 
-# TODO: catch 404 error
+
 START_TIME = time.time()
 
 base_url = "https://understat.com/match"
 
-start_match_id = 81
-total_match_id = 1000
+start_match_id = 1
+total_match_id = 100
 match_ids_range = list(range(start_match_id, total_match_id+1))
 match_ids = list(map(str, match_ids_range))
 
@@ -28,20 +27,19 @@ rfpl_dict = {}
 
 for match_id in match_ids:
     
-    # create the url to call
+    """   create the url to call   """ 
     url = base_url + "/" + match_id 
     
-    """
-    Get scripts based on the given url
-    """
+    """   Get scripts based on the given url   """
     res = requests.get(url)
     soup = BeautifulSoup(res.content, "html.parser")
     scripts = soup.find_all('script')
     
+    """   avoid 404 error that returns empty list   """ 
+    if not scripts:
+        continue
     
-    """
-    Get DATE and LEAGUE NAME - league name is required to iterate through user given match ids
-    """
+    """    Get DATE and LEAGUE NAME - league name is required to iterate through user given match ids   """
     strings = scripts[1].string
     
     ind_start = strings.index("match_info")+26 # TODO: adjust to acquire info in a more elegant way
@@ -53,9 +51,7 @@ for match_id in match_ids:
     date = data['date']
     league_name = data['league']
     
-    """
-    Get YELLOW and RED cards as well as TEAM_IDs for the specific match id
-    """
+    """    Get YELLOW and RED cards as well as TEAM_IDs for the specific match id   """
     strings = scripts[2].string
     ind_start = strings.index("('")+2
     ind_end = strings.index("')")
