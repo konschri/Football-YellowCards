b2f91afe5245152c68d37298bbcb0d22e8d62b77
christantonis.kons <christantonis.kons@gmail.com>
2023-06-13 16:48:19 +0300

    tried glob and defined basic workflow

--- test.py
+++ test.py
@@ -2,11 +2,30 @@
 test for calling objects
 """
 
-from pre_processing import process
+from processing import process
 from BasicML import MLPipeline
+import pandas as pd
+import glob
+
+folder_path = "datasets/merged_datasets/"
+csv_files = glob.glob(folder_path + "*.csv")
+
+dataframes = []
+
+# Iterate over each CSV file
+for file in csv_files:
+    # Read the CSV file as a dataframe
+    df = pd.read_csv(file)
+    # Append the dataframe to the list
+    dataframes.append(df)
+
+
+
 
 a = process("SpanishLaLiga17_18.csv")
 data1 = a.dataPreprocess()
 data2 = a.featureTransformation(1, 4, 7, 18)
+data3 = a.dataPostprocess()
     
-pr = MLPipeline(data2)
+pr = MLPipeline(data3)
+pr.run()
