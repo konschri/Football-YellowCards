dd90857c0821cae0e2a4b478e7f6a26bfee8ab9c
christantonis.kons <christantonis.kons@gmail.com>
2023-07-19 17:21:43 +0300

    added target transformation in PostProcess

--- processing.py
+++ processing.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from statistics import mean
 import pandas as pd
 
 
@@ -15,6 +16,11 @@ class process():
         print(f"The shape of loaded details data: {self.data_details.shape}")
         print(f"The shape of loaded statistics data: {self.data_statistics.shape}")
         
+        self.targetTransformationDict = {
+                "ZeroToTwo": [0, 1, 2],
+                "ThreeOrFour": [3, 4],
+                "FiveOrSix": [5, 6],
+                "SevenOrMore": list(range(7, 20))}
     
     def run(self):
         self.dataPreprocess()
@@ -107,28 +113,28 @@ class process():
         
         # Initialize the new feature columns with zeros
         self.data["past_three_yellow_cards_home"], self.data["past_three_yellow_cards_away"] = 0, 0
-        yellow_cards_count = {team:[] for team in teams}
+        yellow_cards_count = {team:[0] for team in teams}
         
         self.data["past_three_tackles_home"], self.data["past_three_tackles_away"] = 0, 0
-        tackles_count = {team:[] for team in teams}
+        tackles_count = {team:[0] for team in teams}
         
         self.data["past_three_ball_possession_home"], self.data["past_three_ball_possession_away"] = 0, 0
-        ball_possession_count = {team:[] for team in teams}
+        ball_possession_count = {team:[0] for team in teams}
         
         self.data["past_three_possession_lost_home"], self.data["past_three_possession_lost_away"] = 0, 0
-        possession_lost_count = {team:[] for team in teams}
+        possession_lost_count = {team:[0] for team in teams}
         
         self.data["past_three_interceptions_home"], self.data["past_three_interceptions_away"] = 0, 0
-        interceptions_count = {team:[] for team in teams}
+        interceptions_count = {team:[0] for team in teams}
         
         self.data["past_three_fouls_home"], self.data["past_three_fouls_away"] = 0, 0
-        fouls_count = {team:[] for team in teams}
+        fouls_count = {team:[0] for team in teams}
         
         self.data["past_three_duels_won_home"], self.data["past_three_duels_won_away"] = 0, 0
-        duels_won_count = {team:[] for team in teams}
+        duels_won_count = {team:[0] for team in teams}
         
         self.data["past_three_aerials_won_home"], self.data["past_three_aerials_won_away"] = 0, 0
-        aerials_won_count = {team:[] for team in teams}
+        aerials_won_count = {team:[0] for team in teams}
         
         
         
@@ -164,36 +170,36 @@ class process():
                         
         
                 # Assign the yellow cards for the home and away teams as new columns in the dataframe
-                self.data.at[index, 'past_three_yellow_cards_home'] = sum(yellow_cards_count[home_team][-3:])
-                self.data.at[index, 'past_three_yellow_cards_away'] = sum(yellow_cards_count[away_team][-3:])
+                self.data.at[index, 'past_three_yellow_cards_home'] = mean(yellow_cards_count[home_team][-3:])
+                self.data.at[index, 'past_three_yellow_cards_away'] = mean(yellow_cards_count[away_team][-3:])
                 
                 # Assign the tackles for the home and away teams as new columns in the dataframe
-                self.data.at[index, 'past_three_tackles_home'] = sum(tackles_count[home_team][-3:])
-                self.data.at[index, 'past_three_tackles_away'] = sum(tackles_count[away_team][-3:])
+                self.data.at[index, 'past_three_tackles_home'] = mean(tackles_count[home_team][-3:])
+                self.data.at[index, 'past_three_tackles_away'] = mean(tackles_count[away_team][-3:])
                 
                 # Assign the ball possession for the home and away teams as new columns in the dataframe
-                self.data.at[index, 'past_three_ball_possession_home'] = sum(ball_possession_count[home_team][-3:])
-                self.data.at[index, 'past_three_ball_possession_away'] = sum(ball_possession_count[away_team][-3:])
+                self.data.at[index, 'past_three_ball_possession_home'] = mean(ball_possession_count[home_team][-3:])
+                self.data.at[index, 'past_three_ball_possession_away'] = mean(ball_possession_count[away_team][-3:])
                 
                 # Assign the possessions lost for the home and away teams as new columns in the dataframe
-                self.data.at[index, 'past_three_possession_lost_home'] = sum(possession_lost_count[home_team][-3:])
-                self.data.at[index, 'past_three_possession_lost_away'] = sum(possession_lost_count[away_team][-3:])
+                self.data.at[index, 'past_three_possession_lost_home'] = mean(possession_lost_count[home_team][-3:])
+                self.data.at[index, 'past_three_possession_lost_away'] = mean(possession_lost_count[away_team][-3:])
                 
                 # Assign the interceptions for the home and away teams as new columns in the dataframe
-                self.data.at[index, 'past_three_interceptions_home'] = sum(interceptions_count[home_team][-3:])
-                self.data.at[index, 'past_three_interceptions_away'] = sum(interceptions_count[away_team][-3:])
+                self.data.at[index, 'past_three_interceptions_home'] = mean(interceptions_count[home_team][-3:])
+                self.data.at[index, 'past_three_interceptions_away'] = mean(interceptions_count[away_team][-3:])
                 
                 # Assign the fouls for the home and away teams as new columns in the dataframe
-                self.data.at[index, 'past_three_fouls_home'] = sum(fouls_count[home_team][-3:])
-                self.data.at[index, 'past_three_fouls_away'] = sum(fouls_count[away_team][-3:])
+                self.data.at[index, 'past_three_fouls_home'] = mean(fouls_count[home_team][-3:])
+                self.data.at[index, 'past_three_fouls_away'] = mean(fouls_count[away_team][-3:])
                 
                 # Assign the duels won for the home and away teams as new columns in the dataframe
-                self.data.at[index, 'past_three_duels_won_home'] = sum(duels_won_count[home_team][-3:])
-                self.data.at[index, 'past_three_duels_won_away'] = sum(duels_won_count[away_team][-3:])
+                self.data.at[index, 'past_three_duels_won_home'] = mean(duels_won_count[home_team][-3:])
+                self.data.at[index, 'past_three_duels_won_away'] = mean(duels_won_count[away_team][-3:])
                 
                 # Assign the aerials won for the home and away teams as new columns in the dataframe
-                self.data.at[index, 'past_three_aerials_won_home'] = sum(aerials_won_count[home_team][-3:])
-                self.data.at[index, 'past_three_aerials_won_away'] = sum(aerials_won_count[away_team][-3:])
+                self.data.at[index, 'past_three_aerials_won_home'] = mean(aerials_won_count[home_team][-3:])
+                self.data.at[index, 'past_three_aerials_won_away'] = mean(aerials_won_count[away_team][-3:])
                 
                 
                 # Update all the above features for home and away teams
@@ -235,7 +241,7 @@ class process():
         
         ## Drop already processed features
         
-        # self.data = self.data[self.data["Round"] > 5]
+        #self.data = self.data[self.data["Round"] > 1]
         self.data = self.data.drop(["Date", "Location", "Stadium", 
                                     "HomeYellows", "HomeReds", "HomeSecondYellows", 
                                     "AwayYellows", "AwayReds", "AwaySecondYellows",
@@ -259,6 +265,10 @@ class process():
         self.data["Yellows"] = [x + y for x,y in zip(self.data["Yellows"], self.data["secondYellows"])]
         self.data["Reds"] = [x + y for x,y in zip(self.data["Reds"], self.data["secondYellows"])]
         self.data.drop(["secondYellows"], axis=1, inplace=True)
+        
+        self.data["Target"] = self.data["Yellows"].apply(lambda x: next((k for k, v in self.targetTransformationDict.items() if x in v), None))
+        # self.testData["Target"] = self.testData["Yellows"].apply(lambda x: next((k for k, v in self.targetTransformationDict.items() if x in v), None))
+        self.data.drop(["Yellows", "Reds"], axis=1, inplace=True)
         
         return self.data.copy()
         
